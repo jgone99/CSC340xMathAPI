277946d3c6104a5ddf0b23a6270d7eafa4fb0f6c
joshh <joshuahinojognzlz@gmail.com>
2022-09-19 21:53:17 -0400

    first commit

--- src/main/java/com/csc340/RestAPI/RestApiController.java
+++ src/main/java/com/csc340/RestAPI/RestApiController.java
@@ -1,9 +1,5 @@
 package com.csc340.RestAPI;
 
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
-import org.json.JSONArray;
 import org.json.JSONObject;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,63 +13,18 @@ import org.springframework.web.client.RestTemplate;
  */
 @RestController
 public class RestApiController {
-
-    @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format("Hello, %s!", name);
-    }
-
-    /**
-     * Get a random quote from quotable and make it available at this endpoint.
-     *
-     * @return
-     */
-    @GetMapping("/quote")
-    public Object getQuote() {
-        String url = "https://api.quotable.io/random";
+    
+    @GetMapping("/math")
+    public Object getAsteroidData(@RequestParam(value = "operation", defaultValue = "random") String op) {
+        String url = "https://x-math.herokuapp.com/api/" + op;
         RestTemplate restTemplate = new RestTemplate();
-        Object jSonQuote = restTemplate.getForObject(url, Object.class);
-
-        //Print the whole response to console.
-        String quote = restTemplate.getForObject(url, String.class);
-        //Parse out the most important info from the response.
-        JSONObject jo = new JSONObject(quote);
+        Object jSonMath = restTemplate.getForObject(url, Object.class);
+        
+        String mathExp = restTemplate.getForObject(url, String.class);
+        JSONObject jo = new JSONObject(mathExp);
         System.out.println(jo.toString());
-        String quoteAuthor = jo.getString("author");
-        String quoteContent = jo.getString("content");
-        System.out.println(quoteAuthor);
-        System.out.println(quoteContent);
-
-        return jSonQuote;
-    }
-
-    /**
-     * Get a list of universities from hipolabs and parse the most important
-     * info to console.
-     *
-     * @return
-     */
-    @GetMapping("/universities")
-    public List<Object> getUniversities() {
-        String url = "http://universities.hipolabs.com/search?name=sports";
-        RestTemplate restTemplate = new RestTemplate();
-        Object[] universities = restTemplate.getForObject(url, Object[].class);
-
-        //Print the whole response to console
-        JSONArray ja = new JSONArray(universities);
-        System.out.println(ja.toString());
-
-        //Parse out relevant info from each entry in the response
-        Iterator<Object> iter = ja.iterator();
-        while (iter.hasNext()) {
-            JSONObject jo = (JSONObject) iter.next();
-            String name = jo.getString("name");
-            String country = jo.getString("country");
-            System.out.println(name + " : " + country);
-
-        }
-
-        return Arrays.asList(universities);
+        
+        return jSonMath;
     }
 
 }
